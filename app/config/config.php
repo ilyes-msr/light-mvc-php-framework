5dccd2a92b8a10aee5b71bdbe90f27de40fe196c
unknown <ailyes988@gmail.com>
2021-04-19 15:35:30 +0100

    initial commit

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+  define('DB_HOST', 'localhost');
+  define('DB_USER', '_YOUR_USER_');
+  define('DB_PASS', '_YOUR_PASS_');
+  define('DB_NAME', '_YOUR_DB_NAME_');
+
+  define('APPROOT', dirname(dirname(__FILE__)));
+  define('URLROOT', '_YOUR_URL_');
+  define('SITENAME', '_YOUR_SITENAME_');
